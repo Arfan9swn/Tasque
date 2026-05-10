db867e2604372ccde0e6eef0d7db16348a1397b7
aim9xidewinder <arfan76124@gmail.com>
2026-05-10 21:36:39 +0800

    Basic UI Blueprint initiated

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tasque</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body class="bg-slate-800 text-white">
+
+    <!--navbar-->
+    <div class="flex w-[100%] content-center justify-center p-[20px] bg-slate-600">
+        <div class="flex w-[100%] gap-[50px]">
+            <div class="font-bold text-lg justify-center">
+                Tasque
+            </div>
+                <div class="font-semibold flex gap-[50px] justify-center text-center">
+                    <div class="justify-center">
+                        Tentang
+                    </div>
+                    <div class="justify-center">
+                        List
+                    </div>
+                </div>
+        </div>
+    </div>
+
+    <div class="ml-[5%] border rounded-[5px] w-[40vw] mt-[25px]">
+        <input type="text" class="w-[40vw] focus:outline-none px-[10px] py-[3px]">
+    </div>
+    <div class="flex w-[100%] justify-around mt-[25px]">
+        <div class="w-[40%] border rounded-[5px] p-[5px]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam nostrum nobis consequatur, sunt nemo explicabo illo tenetur, officia corrupti, quos earum quidem minima quaerat ut quo nulla omnis quibusdam totam?
+        </div>
+        <div class="w-[40%] border rounded-[5px] p-[5px]">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo aperiam fugiat molestiae consectetur magni magnam et perferendis est non? Iste consectetur itaque velit delectus odio nemo obcaecati ex beatae quo.
+        </div>
+    </div>
+</body>
+</html>
